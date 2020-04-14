56692f4f026c3dda1e935536238b21eb77c7ba99
Martin Prout <mamba2928@yahoo.co.uk>
2020-04-14 11:15:58 +0100

    Fix non working center sliders

--- julia_brot.rb
+++ julia_brot.rb
@@ -20,6 +20,7 @@ class JuliaBrot < Propane::App
   def update_zoom
     @zoom = scaling / width
     @y_range = scaling * height / width
+    @center = Vec2D.new(x_center, y_center)
   end
 
   def setup
